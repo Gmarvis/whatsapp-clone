6191f178324b7e035e2c3a155492355f0a1bcc07
nsamedaisy <nsamedaisy@gmail.com>
2023-11-18 11:16:07 +0100

    conflicts resolution

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,9 @@ export default async function Index() {
 
   return (
     <div className="flex-1 h-[80vh] w-[100%] flex flex-col items-center mx-auto rounded-sm ">
-      <Signup />
+      {/* <Signupb /> */}
+      {/* <Signup /> */}
+      {/* <Discossions /> */}
     </div>
   );
 }
